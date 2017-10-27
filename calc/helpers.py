55e932b32d6721d5141561dddd79686074489e9d
ngrippa <nemo.grippa@dainst.de>
2017-10-27 17:43:05 +0200

    added more helpers

--- calc/helpers.py
+++ calc/helpers.py
@@ -1,5 +1,4 @@
 prec = 0.004
-
 def frange (val , bor1 , bor2):
     """Function is true if a value is between to others. Order of borders doesn't have an impact."""
     if ( (1-prec) * bor1 <= val <= bor2 * (1+prec) or (1-prec) * bor2 <= val <= bor1 * (1+prec) ):
@@ -7,17 +6,35 @@ def frange (val , bor1 , bor2):
     else:
         return False
 
-def UmtoUs (val):
-    """Function converts U/min to U/sec"""
-    res  = val/60
-    return res
+def UmtoUs (Umin):
+    """Function converts U/min to U/sec."""
+    Us  = Umin/60
+    return Us
 
-def mmtom (val):
-    """Function converts mm to m"""
-    res = val/1000
-    return res
+def mmtom (mm):
+    """Function converts mm to m."""
+    m = mm/1000
+    return m
 
-def kWtoW (val):
-    """Function converts kW to W"""
-    res = val * 1000
-    return val
+def mtomm (m):
+    """Function converts m to mm."""
+    mm= m * 1000
+    return mm
+
+
+def kWtoW (kW):
+    """Function converts kW to W."""
+    W = kW * 1000
+    return W
+
+def abst (z1 , z2):
+    """Function calculates the distance between to gears"""
+    from vars import Modul
+    ab =  Modul * (z1 + z2) / 2
+    return ab
+
+def radius (z):
+    """Function calculates the radius of a gear"""
+    from vars import Modul
+    rad = z / Modul
+    return rad
--- calc/main.py
+++ calc/main.py
@@ -4,11 +4,7 @@
 from vars import *
 from helpers import *
 
-# kW -> W
-
-
-
-# Berechnung resultierender Konstanten, alle SI
+# Berechnung resultierender Konstanten,
 DrehAbMax1 = DrehAbMin1 * (1 + DrehAbTol/100)
 DrehAbMax2 = DrehAbMin2 * (1 + DrehAbTol/100)
 AbstW1W2Max = AbstW1W2Min * (1 + AbstW1W2Tol/100)
@@ -21,11 +17,10 @@ UberGesMin2 = DrehAn/DrehAbMin2    # Gesamtübersetzung mindestens Gang 2, 1
 UberGesMax2 = DrehAn/DrehAbMax2    # Gesamtübersetzung maximal Gang 2, 1
 
 # Berechnung Ergebnisse
-# print ("Gültige Zähnezahlpaarungen für Üb 1:")
 for z2 in range(1,300):
-    Abstand12 = Modul * (z1 + z2) / 2
+    Abstand12 = abst(z1 , z2)
     # print (Abstand , AbstW1W2Min , AbstW1W2Max)
-    if ((1-prec) * AbstW1W2Min <= Abstand12 <= AbstW1W2Max * (1+prec)):
+    if (frange(Abstand12 , AbstW1W2Min , AbstW1W2Max)):
         Uber12 = z2/z1
         for z3 in range(1,300):
             Uber23 = z3/z2
@@ -36,10 +31,10 @@ for z2 in range(1,300):
                          Uber45 = z5/z4
                          Uber15 = Uber12 * Uber45
                          if (frange(Uber15 , UberGesMin2 , UberGesMax2)):
-                             Abstand23 = Modul * (z2 + z3) / 2
-                             Abstand45 = Modul * (z4 + z5) / 2
+                             Abstand23 = abst(z2 , z3)
+                             Abstand45 = abst(z4 , z5)
                              if (Abstand45 == Abstand23):
-                                 zahne = [z1 , z2 , z3 , z4 , z5]
+                                 zahne = ["You did something wrong" , z1 , z2 , z3 , z4 , z5]
                                  print ("-----------------------")
                                  print ("Ergebnis \n")
                                  print ("Übersetzung (Gang 1):" , Uber13)
@@ -48,11 +43,11 @@ for z2 in range(1,300):
                                  print ("Abstand der Wellen 2 und 3:" , Abstand23)
                                  print ("Querverschub zwischen z3 und z5:" , Abstand23 - Abstand45 , "\n")
                                  print("Zähnezahlen")
-                                 for i in range(0,5):
-                                     print ("\n Zahnrad" , i+1 , zahne[i])
+                                 for i in range(1,6):
+                                     print ("\n Zahnrad" , i , zahne[i])
                                  print("Durchmesser und Radien in mm")
-                                 for i in range(0,5):
-                                     print ("\n Zahnrad" , i+1 , (zahne[i] / Modul) * 1000)
+                                 for i in range(1,6):
+                                     print ("\n Zahnrad" , i , mtomm(radius(zahne[i])))
                                 # print ("Moment Gang 2")
                                  print ("-----------------------")
 
